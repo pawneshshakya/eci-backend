a92c2c95e050e57ae9e17d1cba81942166e03335
pawneshkumar1 <pawneshkumar162@gmail.com>
2025-04-28 23:14:50 +0530

    add vercel file

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -9,7 +9,7 @@ const protect = async (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, "your-jwt-secret");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id);
     next();
   } catch (err) {
